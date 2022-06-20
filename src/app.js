a8b23edf865e8414ad1cb22495dfe80dfbe8a97c
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-06-20 16:22:19 +0100

    edited

--- src/app.js
+++ src/app.js
@@ -1,11 +1,51 @@
+
+// Task 1 Build an application that allows you to add movies to an
+// array with the possibility optional additional information.
+
+
+// const Movie = require("./utils")
+// const input = process.argv;
+
+// const app = (argvArr) => {
+// if (input[2] === "add") {
+//     const movieObj = new Movie(argvArr[3], argvArr[4]);
+//     movieObj.add();
+// } else if (argvArr[2] === "addMulti") {
+//     const movie1 = new Movie(argvArr[3], argvArr[4]);
+//     const movie2 = new Movie(argvArr[5], argvArr[6]);
+//     movie1.add();
+//     movie2.add();
+// };
+// };
+
+// app(process.argv)
+
+// task 1 end 
+
+// task 2 Build an improved version of the movie app using the yargs command 
+
 const Movie = require("./utils")
-const input = process.argv;
+const yargs = require("yargs");
+
+console.log(yargs.argv);
+const app = (argvArr) => {
+    if (argvArr[2] === "add") {
+        const movieObj = new Movie(argvArr[3], argvArr[4]);
+        movieObj.add();
+    } else if (argvArr[2] === "addmulti") {
+        const movie1 = new Movie(argvArr[3], argvArr[4]);
+        const movie2 = new Movie(argvArr[5], argvArr[6]);
+        movie1.add();
+        movie2.add();
+    };
+};
 
 
-if(input[2] === "add") {
-    const movieObj = new Movie("Spiderman", "Andrew Garfield");
-    movieObj.add();
-}
+
+
+
+
+
 
 // const movies = [];
 // console.log(input)
